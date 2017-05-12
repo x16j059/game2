0e73039cd81027741b7ec3a60490b226d941cc34
WIN_NT\x16j059 <x16j059@chiba-fjb.ac.jp>
2017-05-12 11:30:28 +0900

    途中

---
+++ Example6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //変数の宣言
+            int score;//int型(整数型)
+            double pai;// double型(実数型)
+            String name;//String型(文字列型)
+
+            //変数への代入
+            score = 25;
+            pai = 3.1415;
+            name = "TARO";
+
+            //減数に格納された値を画面に表示
+            Console.WriteLine("SCORE={0}", score);
+            Console.WriteLine("AVE={0}", pai);
+            Console.WriteLine("NAME={0}", name);
+}    
+    }
+}
+
