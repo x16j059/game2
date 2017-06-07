6f318a31ab994c1f8bfb30a644603c9aa6c29ccf
WIN_NT\x16j059 <x16j059@chiba-fjb.ac.jp>
2017-06-07 12:37:09 +0900

    game2

---
+++ game2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game2
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32")] static extern short GetAsyncKeyState(Keys vKey);
+        List<Chara> charaList;
+        TimeCounter timeCount;
+        Chara ship;
+
+        public Form1()
+        {
+              InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            timeCount = new TimeCounter();
+            charaList = new List<Chara>();
+            ship = new Chara(Properties.Resources.ship);
+            charaList.Add(ship);
+        }
+        private void timer1_Tick(object sender, EventArgs e)   {
+            long count = timeCount.getCount();
+            for (long i = 0; i < count; i++)
+            {
+                if (GetAsyncKeyState(Keys.Down) < 0)
+                    ship.Y += 1;
+                if (GetAsyncKeyState(Keys.Up) < 0)
+                    ship.Y -= 1;
+                if (GetAsyncKeyState(Keys.Right) < 0)
+                    ship.x += 1;
+                if (GetAsyncKeyState(Keys.Left) < 0)
+                    ship.x -= 1;
+            }
+            Refresh();
+        }
+        }
+}
---
+++ game2/Properties/SubClass1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game2.Properties
+{
+    class SubClass1
+    {
+    }
+}
+
+//コピペ部分
+namespace Game2
+{
+    class TimeCounter
+    {
+        private Stopwatch sw;
+        private long lastTime;
+        private long ternTime;
+        public TimeCounter()
+        {
+            sw = new System.Diagnostics.Stopwatch();
+            lastTime = sw.ElapsedMilliseconds;
+            ternTime = 3;
+            sw.Start();
+        }
+        public long getCount()
+        {
+            long now = sw.ElapsedMilliseconds;
+            long course = now - lastTime;
+            long count = course / ternTime;
+            lastTime += count * ternTime;
+            return count;
+        }
+
+    }
+    class Chara
+    {
+        public int X;
+        public int Y;
+        public Boolean Visible = true;
+        Bitmap bitmap;
+
+        public Chara(Bitmap b)
+        {
+            bitmap = b;
+        }
+
+        public void draw(Graphics g)
+        {
+            if (Visible)
+                g.DrawImage(bitmap, X, Y);
+        }
+     /* Graphics g = e.Graphics;
+            foreach (Chara chara in charaList)
+            {
+                chara.draw(g);
+            }*/
+
+}
+}
